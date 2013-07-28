b2fee2f1039baf605c0f261e9588dd6c6590cb27
Paul DeMello <pdemello@alumni.utexas.net>
2013-07-27 20:32:40 -0500

    Print DAG

--- cafezinho/util/GraphNode.java
+++ cafezinho/util/GraphNode.java
@@ -4,22 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
  * User: pdemello
  * Date: 7/27/13
  * Time: 5:18 PM CDT
- * To change this template use File | Settings | File Templates.
  */
 public class GraphNode<T> {
     T data;
-    List<GraphNode<T>> children;
+    List<T> edges;
 
     public GraphNode(T data) {
         this.data = data;
-        children = new ArrayList<>();
+        edges = new ArrayList<>();
     }
-    public List<GraphNode<T>> getChildren() {
-        return children;
+    public List<T> getEdges() {
+        return edges;
     }
     public T getData() {
         return data;
--- epi13_7.java
+++ epi13_7.java
@@ -13,6 +13,7 @@ public class epi13_7 {
         Double[][] teams = new Double[n][p];
 
         // Create my teams and sort the players on each team.
+        System.out.printf("All the teams:\n\n");
         for (int i = 0; i < teams.length; ++i) {
             for (int j = 0; j < teams[i].length; ++j) {
                 teams[i][j] = maxHeight * Math.random();
@@ -25,14 +26,40 @@ public class epi13_7 {
             System.out.println();
         }
 
-        // Create my DAG.  Each team is a node.
+        // Create my DAG without any edges.  Each team is a node.
         List<GraphNode<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < teams.length; ++i) {
-            GraphNode<Integer> node = new GraphNode<>(i);
-            graph.add(node);
+        for (int i = 0; i < teams.length; ++i)
+            graph.add(new GraphNode<>(i));
 
+        // Now create my edges.
+        for (int i = 0; i < teams.length; ++i) {
+            for (int j = i + 1; j < teams.length; ++j) {
+                double heightDiff = 0;
+                double prevDiff;
+                boolean noGo = false;
+                // Compare iTeam with jTeam.  We'll either add an edge from i to j, or j to i, or neither.
+                // Compare players in each position... Bomb out if the "taller" team switches from one to the other.
+                for (int player = 0; player < teams[j].length && !noGo; ++player) {
+                    prevDiff = heightDiff;
+                    heightDiff = teams[j][player] - teams[i][player]; // > 0 means j player is taller than i player.
+                    if  (   heightDiff == 0
+                        ||  prevDiff > 0 && heightDiff < 0
+                        ||  prevDiff < 0 && heightDiff > 0)
+                        noGo = true;  // The taller team has switched, these two can't be in the same photo.
+                }
+                if (!noGo)
+                    graph.get(heightDiff > 0 ? j : i).getEdges().add(heightDiff > 0 ? i : j);
+            }
         }
 
+        // DAG complete.  Now just need to find the longest path.  That is the answer.  Print out the graph in the meantime.
+        System.out.printf("\nThe graph:\n\n");
+        for (GraphNode<Integer> node : graph) {
+            System.out.printf("Team\t%d:\t", node.getData());
+            for (int edge : node.getEdges())
+                System.out.printf("%d\t", edge);
+            System.out.println();
+        }
 
         System.out.printf("%d ms\n", (System.nanoTime()-startTime)/100000);
     }
